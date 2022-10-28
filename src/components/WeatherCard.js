b6c1d6de7991fca93c07f4d146f4bdd740510e2e
Nurullah Arslan <codarscoder@gmail.com>
2022-10-28 11:14:17 +0300

    build

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,20 +1,20 @@
 
 
 const WeatherCard = ( {wData} ) => {
-   const iconUrl = `https://openweathermap.org/img/wn/${wData?.weather?.[0].icon}@2x.png`;
+   const iconUrl = `https://openweathermap.org/img/wn/${wData.weather[0].icon}@2x.png`;
   return (
     <li className="city">
       <h2 className="city-name">
-        <span>{wData?.name?.replaceAll("Province", "")}</span>
+        <span>{wData.name.replaceAll("Province", "")}</span>
         <sup>country</sup>
       </h2>
       <div className="city-temp">
-      {Math.round(wData?.main?.temp)}
+      {Math.round(wData.main.temp)}
         <sup>°C</sup>
       </div>
       <figure>
         <img className="city-icon" src={iconUrl} alt={iconUrl} />
-          <figcaption>{wData?.weather?.[0].description}</figcaption> 
+          <figcaption>{wData?.weather[0].description}</figcaption> 
       </figure>
     </li>
   );
